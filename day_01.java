f8839d504d0133f42cf9850f57478bd4dbc8f492
Aied-mercy <1406817094@qq.com>
2022-02-19 21:56:30 +0800

    Java基础

---
+++ day_01.java
@@ -0,0 +1,14 @@
+package 自我理解;
+
+public class day_01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(""+a+b);
+        System.out.println(a+b+"");
+
+
+
+    }
+}
