32f81974d4d3b19e4014ef9dca33e8756da09681
Aied-mercy <1406817094@qq.com>
2022-03-14 23:09:50 +0800

    Java基础代码

---
+++ test_04_if_else语句3.java
@@ -0,0 +1,17 @@
+package day_test_选择结构;
+
+import java.util.Scanner;
+
+public class test_04_if_else语句3 {
+    public static void main(String[] args) {
+        /*int freedom = (int)Math.random()*100;//类型强制转换 高转低 要强制
+        // 因为 所产生的随机数范围为[0.0,1.0) 乘100 变为[0.0,100.0)为浮点型
+        System.out.println("请输入你所购买的号码：");
+        Scanner can = new Scanner(System.in);
+        int guessTicket = can.nextInt();
+        if(guessTicket == freedom)
+            System.out.println("奖金3000美元！");
+        else if(freedom%10==guessTicket%10)
+*/
+    }
+}
