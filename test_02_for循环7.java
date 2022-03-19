5a9a4ded09cf4cbc8d0e00c9acab38d188b134a8
Aied-mercy <1406817094@qq.com>
2022-03-19 23:51:25 +0800

    Java基础代码

---
+++ test_02_for循环7.java
@@ -0,0 +1,10 @@
+package day_test_循环结构;
+
+public class test_02_for循环7 {
+    public static void main(String[] args) {
+        for(int i =1;i<=100;i++)
+        {
+            if()
+        }
+    }
+}
