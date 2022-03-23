169a77803e8465fd342884af9ff5ed91eb9db97f
Aied-mercy <1406817094@qq.com>
2022-03-23 23:06:15 +0800

    Java基础代码

---
+++ StudentTest.java
@@ -0,0 +1,11 @@
+package 面向对象;
+public class StudentTest {
+    public static void main(String[] args) {
+//定义类Student ，包含三个属性：学号number（int），年纪state（int）
+//成绩score（int）。 创建20个学生对象，学号为1到20，年纪和成绩都由随机数确定
+//问题 一 ：打印输出3年纪（state值为3）的学生信息
+//问题 二 ：使用冒泡排序按学生成绩排序，并遍历所有学生信息
+//提示 ：  1）生成随机数：Math.random（），返回值类型为 double
+//        2）四舍五入取整：Math.round（double d），返回值类型 long
+    }
+}
