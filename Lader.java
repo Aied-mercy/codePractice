985921fb2630579908720a9f3ffda11e095e7d51
Aied-mercy <1406817094@qq.com>
2022-04-23 23:07:50 +0800

    Java面向对象

---
+++ Lader.java
@@ -0,0 +1,18 @@
+package 课本练习;
+
+public class Lader {
+    double uplower,height;     //上底、高
+    static double xialower;    //下底
+    void setUplower(double a){
+        uplower = a ;
+    }
+    void setXialower(double b){
+        xialower = b;
+    }
+    double getUplower(){
+        return uplower;
+    }
+    double getXialower(){
+        return xialower;
+    }
+}
