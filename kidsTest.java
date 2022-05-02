96770364bea5fc35d0d030949db7f946537ba658
Aied-mercy <1406817094@qq.com>
2022-05-02 22:34:40 +0800

    Java基础代码

---
+++ kidsTest.java
@@ -0,0 +1,15 @@
+package 面向对象_03_;
+
+public class kidsTest {
+    public static void main(String[] args) {
+        Kids somekid = new Kids(12);
+
+        somekid.printAge();
+
+        somekid.setSalary(0);
+        somekid.setSex(1);
+
+        somekid.employeed();
+        somekid.manorWoman();
+    }
+}
