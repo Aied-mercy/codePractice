bb624d452d4f94b0418a66ed3c07eb97fe871bf3
Aied-mercy <1406817094@qq.com>
2022-07-09 22:03:24 +0800

    Java面向对象

---
+++ A_02_abstract关键字_CommonEmployee.java
@@ -0,0 +1,9 @@
+package Java基础_05;
+
+public class A_02_abstract关键字_CommonEmployee extends A_02_abstract关键字_Employee{
+    @Override
+    public void work(){
+        System.out.println("员工在一线车间生产高质量的产品！");
+    }
+
+}
