dea60f937786a38223fa1c6fd78416ed7ece7f6d
Aied-mercy <1406817094@qq.com>
2022-07-11 22:49:29 +0800

    Java面向对象

---
+++ A_03_TemplateTest.java
@@ -0,0 +1,36 @@
+package Java基础_05;
+//模板方法
+public class A_03_TemplateTest {
+    public static void main(String[] args) {
+        SubTemplate t = new SubTemplate();
+        t.spendTime();
+    }
+}
+abstract class  Template{
+    //计算某段代码执行所需要花费的时间
+    public void spendTime(){
+        long start = System.currentTimeMillis();
+        this.code();  //不确定的部分、易变的部分
+        long end = System.currentTimeMillis();
+        System.out.println("花费的时间为：" + ( end - start ));
+    }
+    public abstract void code();
+}
+class SubTemplate extends Template{
+    @Override
+    public void code(){
+        for(int i = 2;i<=1000;i++){
+            boolean isFlag = true;
+            for(int j = 2;j<=Math.sqrt(i);j++)
+            {
+                if(i%j == 0){
+                    isFlag = false;
+                }
+            }
+            if(isFlag) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
