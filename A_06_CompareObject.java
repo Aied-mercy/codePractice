0bf36b7da845a158b8876272e6a296827e023d79
Aied-mercy <1406817094@qq.com>
2022-07-14 22:25:52 +0800

    Java面向对象

---
+++ A_06_CompareObject.java
@@ -0,0 +1,11 @@
+package Java基础_05;
+/*
+interface CompareObject{
+  public int compareTo(object o);
+    //若返回值是0，代表相等；若为正数，代表当前对象大；负数代表当前对象小
+}
+ */
+public interface A_06_CompareObject{
+    //若返回值是0，代表相等；若为正数，代表当前对象大；负数代表当前对象小
+    public int compareTo(Object o);
+}
