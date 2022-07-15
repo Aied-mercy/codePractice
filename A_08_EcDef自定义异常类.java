68ef4f5a8e93bb147af912df9c5fe158dc7dece8
Aied-mercy <1406817094@qq.com>
2022-07-15 22:39:51 +0800

    Java面向对象

---
+++ A_08_EcDef自定义异常类.java
@@ -0,0 +1,14 @@
+package Java基础_05;
+
+public class A_08_EcDef自定义异常类 extends Exception{
+    //序列版本号
+    static final long serialVersionUID = -32224357746L;
+
+    public A_08_EcDef自定义异常类(){
+
+    }
+    public A_08_EcDef自定义异常类(String msg ){
+        super(msg);
+    }
+
+}
